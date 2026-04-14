a543409bd1ca921855816d98890d316f00acecd9
Jhon Kleber Zambrano Macias <jhonmacias1999@gmail.com>
2026-04-15 05:03:14 +0800

    Envido de datos del sensor en tiempo real

--- samples/sample_c/platform/linux/raspberry_pi/application/dji_sdk_config.h
+++ samples/sample_c/platform/linux/raspberry_pi/application/dji_sdk_config.h
@@ -52,7 +52,7 @@ extern "C" {
 
 #define CONFIG_MODULE_SAMPLE_WIDGET_ON                      true
 
-#define CONFIG_MODULE_SAMPLE_SENSOR_SIM_ON                  false
+#define CONFIG_MODULE_SAMPLE_SENSOR_SIM_ON                  true
 
 
 #define CONFIG_MODULE_SAMPLE_WIDGET_SPEAKER_ON              true
